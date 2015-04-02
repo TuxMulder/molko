713c62cbe183295e08bcf120c14ebd170ea7ae10
Mitchell Eccles <mitchell.eccles@gmail.com>
2015-04-02 16:01:57 +0000

    Tweaked data cleaning script and obtained 1500 records

---
+++ data_loader/info.sql
@@ -0,0 +1,14 @@
+0	OrganisationCode,
+1	Name,
+2	NationalGrouping,
+3	HighLevelHealthGeography,
+4	Address 1
+5	Address 2
+6	Address 3
+7	Address 4
+8	Address 5,
+9	PostCode,
+12	StatusCode -> WHERE ="A" - "P" is another option; proposed, but is less useful,
+13	OrganisationSubTypeCode -> WHERE = 1; 1=Pharmacy, 2 = Appliance Contractor, 3 = Oxygen concentrator supplier
+17	ContactTelephone,
+23	CurrentCareOrganisation <- not sure what it is yet, but it could link to a Primary Care Organisation, which might be useful down the line
