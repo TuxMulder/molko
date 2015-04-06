7c7daf9499ed50382ec9d4efa16a46d37839bf30
Mitchell Eccles <mitchell.eccles@gmail.com>
2015-04-06 11:57:06 +0100

    added faster post code to lat/long lookup

--- data_loader/DataCleaner.php
+++ data_loader/DataCleaner.php
@@ -23,9 +23,24 @@ fclose($outJson);
 fclose($handle);
 
 function convertPostCodeToLatLong($postCode){
+	try{		
+		$url = "http://api.postcodes.io/postcodes/$postCode";
+		$data = file_get_contents($url);
+		$postCodeData = json_decode(file_get_contents($url));
+		$lat = $postCodeData -> {'result'} ->{'latitude'};
+		$long = $postCodeData -> {'result'} ->{'longitude'};
+
+		return "[$lat,$long]";
+	}
+	catch(Exception $e){
+		return slowerConvertPostCodeToLatLong($postCode);
+	}
+}
+
+function slowerConvertPostCodeToLatLong($postCode){
 	try{
 		$postCode = str_replace(" ", "", $postCode);
-		$url = "http://uk-postcodes.com/postcode/$postCode.csv"; 
+		$url = "http://uk-postcodes.com/postcode/$postCode.csv";
 		$postCodeData = explode(",", file_get_contents($url));
 		return "[$postCodeData[1], $postCodeData[2]]";
 	}
@@ -35,24 +50,28 @@ function convertPostCodeToLatLong($postCode){
 	}
 }
 
+function formatPostCode($postCode){
+	return str_replace(" ", "", $postCode);	
+}
+
 function convertLineToJson($line){
 	$jsonRecord = 
 	"{" . 
-		"OrgCode: \"$line[0]\"," .
-		"Name: \"$line[1]\"," .
-		"NationalGrouping: \"$line[2]\"," .
-		"HighLevelHealthGeo: \"$line[3]\"," .
-		"Address1: \"$line[4]\"," .
-		"Address2: \"$line[5]\"," .
-		"Address3: \"$line[6]\"," .
-		"Address4: \"$line[7]\"," .
-		"Address5: \"$line[8]\"," .
-		"loc:{type: \"Point\", coordinates: " . convertPostCodeToLatLong($line[9]) . "}," .
-		"PostCode: \"$line[9]\"," .
-		"StatusCode: \"$line[12]\"," .
-		"OrgSubTypeCode: \"$line[13]\"," .
-		"Tel: \"$line[17]\"," .
-		"CurrentCareOrg: \"$line[23]\"" .
+		"\"OrgCode\": \"$line[0]\"," .
+		"\"Name\": \"$line[1]\"," .
+		"\"NationalGrouping\": \"$line[2]\"," .
+		"\"HighLevelHealthGeo\": \"$line[3]\"," .
+		"\"Address1\": \"$line[4]\"," .
+		"\"Address2\": \"$line[5]\"," .
+		"\"Address3\": \"$line[6]\"," .
+		"\"Address4\": \"$line[7]\"," .
+		"\"Address5\": \"$line[8]\"," .
+		"\"loc\":{\"type\": \"Point\", \"coordinates\": " . convertPostCodeToLatLong(formatPostCode($line[9])) . "}," .
+		"\"PostCode\": \"$line[9]\"," .
+		"\"StatusCode\": \"$line[12]\"," .
+		"\"OrgSubTypeCode\": \"$line[13]\"," .
+		"\"Tel\": \"$line[17]\"," .
+		"\"CurrentCareOrg\": \"$line[23]\"" .
 	"}";
 	return $jsonRecord;
 }
